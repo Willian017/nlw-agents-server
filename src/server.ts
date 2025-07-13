8d9e8b8e16e98e1759eee72ad9a9745eb8a8e677
willian017 <willianyuji100@gmail.com>
2025-07-13 16:41:01 -0300

    feat: gemini api integration and vector search

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastify } from 'fastify'
+import { fastifyMultipart } from '@fastify/multipart'
 import { 
     serializerCompiler,
     validatorCompiler,
@@ -6,17 +7,19 @@ import {
 } from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
-import { sql } from './db/connection.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts'
 import { createQuestionRoute } from './http/routes/create-question.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
     origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -29,5 +32,6 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestionsRoute)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({port: env.PORT})
